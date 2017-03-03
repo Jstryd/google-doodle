a802d8b22a1d05c70b5308fab3157849661c5a5e
Jstryd <jms44stryd@yahoo.com>
2017-03-02 23:17:42 -0800

    i hate everything

--- js/plugin.js
+++ js/plugin.js
@@ -1,1 +1,12 @@
 // Paste jQuery Plugin Code Here
+
+// DrawSVG 
+var google = $('svg').drawsvg({
+  duration: 5000,
+  reverse: true,
+  stagger: 1000,
+});
+
+google.drawsvg('animate');
+
+//All other animations should be done in CSS
